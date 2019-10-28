958fa7944af02af70c223b5204997ceca0d386ba
Eric Shu <eric.shu@aelf.io>
2019-10-28 18:10:42 +0800

    update log parse script

--- script/parse_log.sh
+++ script/parse_log.sh
@@ -2,16 +2,16 @@
 
 #scp ubuntu@$1:/opt/node-1/Logs/2019-10-26.log ./
 log_path='/opt/node-1/Logs'
-date=`date '+%Y-%m-%d'`
-echo '=>parse ${date} log'
+date_info=`date '+%Y-%m-%d'`
+echo "=>parse ${date_info} log"
 
 if [[ ! -d ./log  ]];then
   mkdir ./log
 fi
 
-grep "Generated block" ${log_path}/${date}.log |grep -v "grep" |awk '{print $1, $2, substr($11,2,64), $13, substr($16,2,64), $19, $23}' >./log/gen-blocks.log
-grep "WARN" ${log_path}/${date}.log |grep -v "grep" >./log/warn.log
-grep "ERROR" ${log_path}/${date}.log |grep -v "grep" >./log/error.log
+grep "Generated block" ${log_path}/${date_info}.log |grep -v "grep" |awk '{print $1, $2, substr($11,2,64), $13, substr($16,2,64), $19, $23}' >./log/gen-blocks.log
+grep "WARN" ${log_path}/${date_info}.log |grep -v "grep" >./log/warn.log
+grep "ERROR" ${log_path}/${date_info}.log |grep -v "grep" >./log/error.log
 
 echo "log handle completed"
 echo ""
